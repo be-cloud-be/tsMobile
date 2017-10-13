07fa6d41f2e023e9e3303f00a12204a1ca76ce60
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-13 09:05:09 +0200

    add pad

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
+import { IonPasscodeOptions } from './ion-passcode';
 
 @NgModule({
   declarations: [
