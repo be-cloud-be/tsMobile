e4ea5623ca774f2135006a6dcf5347bc70a44310
be-cloud-js <jerome.sonnet@be-cloud.be>
2017-10-14 17:17:16 +0200

    first form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
+import { LoadingController, IonicPage, NavController } from 'ionic-angular';
 
 
 @IonicPage({
@@ -11,8 +14,33 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  timesheet : FormGroup;
 
+  sites = [
+    { id: 'bissen', name : 'Bissen' },
+    { id: 'mamer', name : 'Mamer'},
+    { id: 'jalhay', name : 'Jalhay'}
+  ];
+
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, private formBuilder: FormBuilder) {
+    this.timesheet = this.formBuilder.group({
+      date: [new Date().toISOString(), Validators.required],
+      site: ['', Validators.required],
+      start: ['7:30'],
+      end: ['16:30'],
+      pause: ['00:30'],
+    });
+  };
+
+  logForm() {
+    if(this.timesheet.valid){
+      // (optional) show a message to your users while you are verifying the passcode
+      let loader = this.loadingCtrl.create({ content: 'Pointage envoyé : ' + JSON.stringify(this.timesheet.value), dismissOnPageChange: true });
+      loader.present();
+      setTimeout(() => {
+        loader.dismiss();
+      }, 2000);
+    }
   }
 
 }
