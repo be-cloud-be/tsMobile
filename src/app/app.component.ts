58c06e68872fbd3a869f2eaae185df2c79e7d6b1
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-17 09:23:33 +0200

    translate to fr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Config, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TranslateService } from '@ngx-translate/core';
+import { MomentModule } from 'angular2-moment';
 
 import { LoginPage } from '../pages/login/login';
 
-import moment from "moment";
-import "moment/src/locale/fr";
+import * as moment from 'moment';
 
 @Component({
   templateUrl: 'app.html'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
 import { TranslateModule } from '@ngx-translate/core';
+import { MomentModule } from 'angular2-moment';
 
 import { TSMobile } from './app.component';
 import { LoginPageModule } from '../pages/login/login.module';
@@ -22,6 +23,7 @@ import { OdooProvider } from '../providers/odoo/odoo';
     IonicModule.forRoot(TSMobile, OdooProvider),
     HttpModule,
     TranslateModule.forRoot(),
+    MomentModule,
     LoginPageModule,
     HomePageModule,
     SubmitPageModule,
