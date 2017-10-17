4fae138c2ebee073e9b7b27a72a38cea65e5f637
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-17 10:00:46 +0200

    add mobile code on employee

--- odoo/tsMobile/controllers/main.py
+++ odoo/tsMobile/controllers/main.py
@@ -15,6 +15,16 @@ from odoo.tools import html2plaintext
 class TsMobile(http.Controller):
 
     @http.route([
+        '/check_code'
+    ], type='json', auth='public', website=True, csrf=False, cors="*")
+    def sites(self, userCode, debug=False, **k):
+        user_ids = request.env['hr.employee'].sudo().search_read([['mobile_code','=',userCode]],['name'])
+        if user_ids:
+            return { "name" : user_ids[0].name}
+        else
+            return { "code": -32600, "message": "Invalid Request"}
+
+    @http.route([
         '/sites'
     ], type='json', auth='public', website=True, csrf=False, cors="*")
     def sites(self, userCode=False, debug=False, **k):
