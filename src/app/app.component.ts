0716102b2685b3c62c5c65e051bace4f723eb2ac
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-17 09:08:45 +0200

    translate to fr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Config, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TranslateService } from '@ngx-translate/core';
 
 import { LoginPage } from '../pages/login/login';
+
+import moment from "moment";
+import "moment/src/locale/fr";
 
 @Component({
   templateUrl: 'app.html'
@@ -12,13 +15,18 @@ import { LoginPage } from '../pages/login/login';
 export class TSMobile {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translateService: TranslateService) {
+  constructor(config: Config, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translateService: TranslateService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
       translateService.setDefaultLang('fr');
+      moment.locale('fr');
+      config.set('monthNames', moment.months())
+      config.set('monthShortNames', moment.monthsShort())
+      config.set('dayNames', moment.weekdays())
+      config.set('dayShortNames', moment.weekdaysShort())
     });
   }
 }
