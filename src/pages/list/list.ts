2f0fe90141a46237893eb183b0dba3f7631f9b21
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-18 07:44:45 +0200

    save encoding details in analytic lines

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, List } from 'ionic-angular';
 import { OdooProvider, ISubmission } from '../../providers/odoo/odoo';
 
 /**
