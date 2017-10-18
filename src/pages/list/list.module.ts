6d1319bb65f6e44f011913eca4b825e8787d2b32
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-18 07:49:33 +0200

    save encoding details in analytic lines

--- src/pages/list/list.module.ts
+++ src/pages/list/list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ListPage } from './list';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ListPage } from './list';
   ],
   imports: [
     IonicPageModule.forChild(ListPage),
+    MomentModule,
   ],
 })
 export class ListPageModule {}
